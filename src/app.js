573ce2a6dfa0953abfc377da0b749aa0e5319f67
Animeshhit <128497576+Animeshhit@users.noreply.github.com>
2025-01-14 19:48:20 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://memogram-v2.vercel.app/"],
     }),
 );
 
